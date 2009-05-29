389194c46dfd73346d501cf15717be8890cdc663
Marc Munro <marc@bloodnok.com>
2009-05-29 16:24:15 -0700

    Addition of relaxng validation and the beginnings of tsort.
    Relaxng is currently being held off as there are issues with getting decent
    error messages from recursively defined schemas.
    
    Tsort is implemented as far as the initial creation and sorting of DagNodes.
    Next, we have to actually generate the output document with all of the
    necessary visit nodes, etc.

---
+++ test/check_relaxng.c
@@ -0,0 +1,187 @@
+/**
+ * @file   check_relaxng.c
+ * \code
+ *     Author:       Marc Munro
+ *     Fileset:	skit - a database schema management toolset
+ *     Author:  Marc Munro
+ *     License: GPL V3
+ * $Id$
+ * \endcode
+ * @brief  
+ * Unit tests for checking relaxng validation of templates, etc
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+#include <regex.h>
+#include "../src/skit_lib.h"
+#include "../src/exceptions.h"
+#include "suites.h"
+
+Document *
+getDoc(char *name)
+{
+    String *docname = stringNew(name);
+    Document *doc = findDoc(docname);
+    objectFree((Object *) docname, TRUE);
+    return doc;
+}
+
+static int
+validateDoc(Document *doc, Document *rng_doc)
+{
+    int result;
+    xmlRelaxNGParserCtxtPtr ctx;
+    xmlRelaxNGPtr schema;
+    xmlRelaxNGValidCtxtPtr validator;
+
+    (void) xmlRelaxNGInitTypes();
+    ctx = xmlRelaxNGNewDocParserCtxt(rng_doc->doc);
+    schema = xmlRelaxNGParse(ctx);
+    validator = xmlRelaxNGNewValidCtxt(schema);
+
+    result = xmlRelaxNGValidateDoc(validator, doc->doc);
+
+    xmlRelaxNGFreeValidCtxt(validator);
+    xmlRelaxNGFree(schema);
+    xmlRelaxNGFreeParserCtxt(ctx);
+    (void) xmlRelaxNGCleanupTypes();
+    return result;
+}
+
+static int
+validate(char *doc_str, char *rng_str)
+{
+    Document *doc_template;
+    Document *rng_doc;
+    int result;
+
+    rng_doc = getDoc(rng_str);
+    doc_template = getDoc(doc_str);
+    //dbgSexp(doc_template);
+    result = validateDoc(doc_template, rng_doc);
+
+    objectFree((Object *) rng_doc, TRUE);
+    objectFree((Object *) doc_template, TRUE);
+
+    return result;
+}
+
+START_TEST(check_list)
+{
+    Document *list_template;
+    Document *rng_doc;
+    int result;
+
+    initBuiltInSymbols();
+    initTemplatePath(".");
+
+    result = validate("list.xml", "template.rng");
+    fail_if(result != 0,
+	    "check_list: list.xml fails to validate");
+
+    FREEMEMWITHCHECK;
+}
+END_TEST
+
+START_TEST(check_deps)
+{
+    Document *list_template;
+    Document *rng_doc;
+    int result;
+
+    initBuiltInSymbols();
+    initTemplatePath(".");
+
+    result = validate("add_deps.xml", "template.rng");
+    fail_if(result != 0,
+	    "check_deps: add_deps.xml fails to validate");
+
+    FREEMEMWITHCHECK;
+}
+END_TEST
+
+START_TEST(check_connect)
+{
+    Document *list_template;
+    Document *rng_doc;
+    int result;
+
+    initBuiltInSymbols();
+    initTemplatePath(".");
+
+    result = validate("connect.xml", "template.rng");
+    fail_if(result != 0,
+	    "check_connect: connect.xml fails to validate");
+
+    FREEMEMWITHCHECK;
+}
+END_TEST
+
+static void
+evalStr(char *str)
+{
+    char *tmp = newstr(str);
+    Object *obj = evalSexp(tmp);
+    objectFree(obj, TRUE);
+    skfree(tmp);
+}
+
+
+START_TEST(check_extract)
+{
+    Document *list_template;
+    Document *rng_doc;
+    int result;
+
+    initBuiltInSymbols();
+    initTemplatePath(".");
+    evalStr("(setq dbver (version '8.3'))");
+
+    result = validate("zzz.xml", "template.rng");
+    fail_if(result != 0,
+	    "check_extract: extract.xml fails to validate");
+
+    FREEMEMWITHCHECK;
+}
+END_TEST
+
+START_TEST(check_test2)
+{
+    Document *list_template;
+    Document *rng_doc;
+    int result;
+
+    initBuiltInSymbols();
+    initTemplatePath(".");
+    evalStr("(setq dbver (version '8.3'))");
+
+    result = validate("test.xml", "test.rng");
+    fail_if(result != 0,
+	    "check_test: test.xml fails to validate");
+
+    FREEMEMWITHCHECK;
+}
+END_TEST
+
+Suite *
+relaxng_suite(void)
+{
+    Suite *s = suite_create("RelaxNG");
+
+    /* Core test case */
+    TCase *tc_core = tcase_create("RelaxNGx");
+    ADD_TEST(tc_core, check_list);
+    ADD_TEST(tc_core, check_deps);
+    ADD_TEST(tc_core, check_connect);
+    //ADD_TEST(tc_core, check_extract);
+    //ADD_TEST(tc_core, check_test2);
+				
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+
---
+++ test/check_tsort.c
@@ -0,0 +1,149 @@
+/**
+ * @file   check_tsort.c
+ * \code
+ *     Author:       Marc Munro
+ *     Fileset:	skit - a database schema management toolset
+ *     Author:  Marc Munro
+ *     License: GPL V3
+ * $Id$
+ * \endcode
+ * @brief  
+ * Unit tests for checking dependency sorting functions
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <check.h>
+#include <regex.h>
+#include "../src/skit_lib.h"
+#include "../src/exceptions.h"
+#include "suites.h"
+
+static Object *
+showDeps(Object *node_entry, Object *dagnodes)
+{
+    DagNode *node = (DagNode *) ((Cons *) node_entry)->cdr;
+    int i;
+    printSexp(stderr, "NODE: ", (Object *) node);
+    if (node->dependencies) {
+	for (i = 0; i < node->dependencies->elems; i++) {
+	    printSexp(stderr, "   --> ", node->dependencies->vector[i]);
+	}
+    }
+
+    return (Object *) node;
+}
+
+static void
+check_build_order(Vector *results, char *list_str)
+{
+    Object *tmp;
+    String *name;
+    DagNode *node;
+    Cons *list = (Cons *) objectFromStr(list_str);
+    assert(list,
+	   "check_build_order: failed to build list");
+    assert(list->type == OBJ_CONS,
+	   "check_build_order: parameter must be a list");
+    assert(list->car,
+	   "check_build_order: parameter must have contents");
+
+    int i = 0;
+    while (list) {
+	name = (String *) list->car;
+	assert(name->type == OBJ_STRING,
+	       "check_build_order: list item is not a string");
+
+	while (i < results->elems) {
+	    node = (DagNode *) results->vector[i];
+	    if (streq(node->fqn->value, name->value)) {
+		/* We have a match, go on to the next name */
+		break;
+	    }
+	    i++;
+	    if (i >= results->elems) {
+		/* The last name in the list was not found. */
+		fail(newstr("Error: %s is out of order", name->value));
+	    }
+	}
+
+	tmp = (Object *) list;
+	list = (Cons *) list->cdr;
+	objectFree((Object *) name, TRUE);
+	objectFree(tmp, FALSE);
+    }
+}
+
+
+START_TEST(check_gensort)
+{
+    Document *doc;
+    Vector *results;
+    Object *ignore;
+    char *tmp;
+    int result;
+
+    initBuiltInSymbols();
+    initTemplatePath(".");
+    ignore = evalSexp(tmp = newstr("(setq build t)"));
+    objectFree(ignore, TRUE);
+    skfree(tmp);
+    ignore = evalSexp(tmp = newstr("(setq drop t)"));
+    objectFree(ignore, TRUE);
+    skfree(tmp);
+
+    doc = getDoc("test/data/gensource1.xml");
+    results = gensort(doc);
+    printSexp(stderr, "RESULTS: ", (Object *) results);
+    check_build_order(results, "('drop.role.cluster.keep' 'drop.cluster' "
+		      "'build.cluster' 'build.role.cluster.keep')");
+    check_build_order(results, "('drop.role.cluster.keep2' 'drop.cluster' "
+		      "'build.cluster' 'build.role.cluster.keep2')");
+    check_build_order(results, "('drop.role.cluster.lose' 'drop.cluster' "
+		      "'build.cluster' 'build.role.cluster.lose')");
+    check_build_order(results, "('drop.role.cluster.marc' 'drop.cluster' "
+		      "'build.cluster' 'build.role.cluster.marc')");
+    check_build_order(results, "('drop.role.cluster.marco' 'drop.cluster' "
+		      "'build.cluster' 'build.role.cluster.marco')");
+    check_build_order(results, "('drop.role.cluster.wibble' 'drop.cluster' "
+		      "'build.cluster' 'build.role.cluster.wibble')");
+    check_build_order(results, "('drop.grant.cluster.lose.keep:keep' "
+		      "'build.role.cluster.lose' "
+		      "'build.grant.cluster.lose.keep:keep')");
+    check_build_order(results, "('build.role.cluster.keep' "
+		      "'build.grant.cluster.lose.keep:keep')");
+    check_build_order(results, 
+		      "('drop.grant.cluster.tbs2.create:keep2:regress' "
+		      "'drop.tablespace.cluster.tbs2' "
+		      "'build.role.cluster.regress' "
+		      "'build.tablespace.cluster.tbs2' "
+	              "'build.grant.cluster.tbs2.create:keep2:regress')");
+    check_build_order(results, 
+		      "('drop.grant.cluster.tbs2.create:keep2:regress' "
+		      "'drop.tablespace.cluster.tbs2' "
+		      "'build.role.cluster.keep2' "
+		      "'build.tablespace.cluster.tbs2' "
+	              "'build.grant.cluster.tbs2.create:keep2:regress')");
+
+    objectFree((Object *) results, TRUE);
+    objectFree((Object *) doc, TRUE);
+    FREEMEMWITHCHECK;
+}
+END_TEST
+
+Suite *
+tsort_suite(void)
+{
+    Suite *s = suite_create("tsort");
+
+    /* Core test case */
+    TCase *tc_core = tcase_create("tsort");
+    ADD_TEST(tc_core, check_gensort);
+				
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
+
