34a4432e8c3b6e94f66edf3adbc84f00020b11df
Marc Munro <marc@bloodnok.com>
2010-12-24 08:20:37 -0800

    Bugfix: Made alltypes return the base type for arrays rather than the array type.

--- templates/postgres/8.3/sql/alltypes.sql
+++ templates/postgres/8.3/sql/alltypes.sql
@@ -1,13 +1,22 @@
--- List all types by oid to get name and schema
--- Todo: add a basename field.  For array types, this will be the name
--- of the underlying object type, for others it will be the same as name.
+-- List all types by oid to get name and schema for use in dependencies.
+-- If the type is an array type, the dependency will on the underlying
+-- type, not the array type itself, so in this case we convert the name
+-- to that of the base type for the array.
+
 select t.oid as oid,
-       t.typname as name,
-       n.nspname as schema
+       coalesce(tb.typname, t.typname) as name,
+       coalesce(nb.nspname, n.nspname) as schema
 from   pg_catalog.pg_type t
-       inner join
-          pg_catalog.pg_namespace n
-       on (n.oid = t.typnamespace);
+inner join 
+       pg_catalog.pg_namespace n
+    on n.oid = t.typnamespace
+left outer join (
+           pg_catalog.pg_type tb
+    inner join 
+           pg_catalog.pg_namespace nb
+        on nb.oid = tb.typnamespace)
+    on tb.typarray = t.oid
+where t.typname like '%threestr';
 
 
 
